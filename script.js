5c281e68de28a7fa354376e37f5d3327a93fd1ad
sxie17 <55156552+sxie17@users.noreply.github.com>
2020-04-17 17:03:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+/** Add any JavaScript you need to this file. */
+
+function fn1(){
+  var t = document.getElementById('snacksss1');
+    var a = document.getElementById('snacksss2');
+    var b = document.getElementById('snacksss3');
+    var c = document.getElementById('snacksss4');
+    var d = document.getElementById('snacksss5'); 
+    var e = document.getElementById('snacksss6');
+    if(t.style.visibility === 'hidden') {
+        t.style.visibility = 'visible';
+        a.style.visibility = 'visible';
+        b.style.visibility = 'visible';
+        c.style.visibility = 'visible';
+        d.style.visibility = 'visible';
+        e.style.visibility = 'visible';
+    } else {
+        t.style.visibility = 'hidden';
+        a.style.visibility = 'hidden';
+        b.style.visibility = 'hidden';
+        c.style.visibility = 'hidden';
+        d.style.visibility = 'hidden';
+        e.style.visibility = 'hidden';
+    }
+}
+
+  function fn2(){
+    var t = document.getElementById('drinksss1');
+    var a = document.getElementById('drinksss2');
+    var b = document.getElementById('drinksss3');
+    var c = document.getElementById('drinksss4');
+    var d = document.getElementById('drinksss5'); 
+    if(t.style.visibility === 'hidden') {
+        t.style.visibility = 'visible';
+        a.style.visibility = 'visible';
+        b.style.visibility = 'visible';
+        c.style.visibility = 'visible';
+        d.style.visibility = 'visible';
+    } else {
+        t.style.visibility = 'hidden';
+        a.style.visibility = 'hidden';
+        b.style.visibility = 'hidden';
+        c.style.visibility = 'hidden';
+        d.style.visibility = 'hidden';
+    }
+  }
+
